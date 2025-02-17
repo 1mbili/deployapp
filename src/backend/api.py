8761b6329d5011e68ae03902ca3dbb2afe6cada4
Michal Biliński <m.bilinskimichal@gmail.com>
2025-02-17 23:25:06 +0100

    Add storing tags in cosmosDB

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,7 +1,11 @@
-from fastapi import Request, status, APIRouter, UploadFile, File, HTTPException
+from fastapi import Request, status, APIRouter, UploadFile, File, HTTPException, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from src.backend.azure_blob_handler import AzureBlobHandler
+from src.backend.azure_cosmosdb_handler import AzureCosmosDBHandler
+from typing import List, Optional
+
+import uuid
 
 templates = Jinja2Templates(directory="src/templates")
 
@@ -26,12 +30,28 @@ async def upload_image(request: Request):
 
 
 @router.post("/upload", status_code=status.HTTP_201_CREATED)
-async def post_recipt_image(request: Request, file: UploadFile = File(...)):
+async def post_recipt_image(request: Request, 
+                            file: UploadFile = File(...),
+                            tag_key: Optional[List[str]] = Form(None),
+                            tag_value: Optional[List[str]] = Form(None)):
     if file.content_type not in ["application/pdf", "image/png", "image/jpg", "image/jpeg"]:
         raise HTTPException(400, detail="Invalid file type")
     content = await file.read()
-    container_prefix = f"prod/{request.headers['x-ms-client-principal-name']}"
+    if "x-ms-client-principal-name" in request.headers:
+        container_prefix = f"prod/{request.headers['x-ms-client-principal-name']}"
+        user_id = request.headers['x-ms-client-principal-id']
+    else: 
+        container_prefix = "dev"
+        user_id = "dev"
     az_handler = AzureBlobHandler(prefix=container_prefix)
     print("Uploading file")
     az_handler.upload_blob(file.filename, content)
+    print("File Uploaded succesfully") 
+    cosmos_handler = AzureCosmosDBHandler()
+    tags = {"id": uuid.uuid4().hex, "userId": user_id, "fileName": file.filename}
+    print(tag_key, tag_value)
+    if tag_key and tag_value:
+        user_tags = {key: value for key, value in zip(tag_key, tag_value) if key and value}
+        tags.update(user_tags)
+    cosmos_handler.upload_document(tags)
     return "OK"
---
+++ src/backend/azure_cosmosdb_handler.py
@@ -0,0 +1,45 @@
+import os
+from azure.identity import DefaultAzureCredential
+from azure.cosmos import CosmosClient, exceptions, PartitionKey
+
+import logging
+logger = logging.getLogger(__name__)
+
+DATABASE_NAME = 'zdjecia'
+
+class AzureCosmosDBHandler():
+    """
+    Class to handle all the blob operations
+    """
+
+    def __init__(self, container_name: str = "metadata", prefix: str = "tests/"):
+        """
+        Initialize the class with the container name
+        :param container_name: The name of the container
+        """
+        self.container_name = container_name
+        self.prefix = prefix
+        if conn_str := os.getenv("COSMOS_DB_KEY"):
+            self.client = CosmosClient.from_connection_string(conn_str)
+        else:
+            default_credential = DefaultAzureCredential()
+            self.client = CosmosClient(container_name=container_name,
+                                             credential=default_credential)
+        try:
+            self.database = self.client.create_database(DATABASE_NAME)
+        except exceptions.CosmosResourceExistsError:
+            self.database = self.client.get_database_client(DATABASE_NAME)
+
+        try:
+            self.container = self.database.create_container(id=self.container_name, partition_key=PartitionKey(path="/userId"))
+        except exceptions.CosmosResourceExistsError:
+            self.container = self.database.get_container_client(self.container_name)
+        
+        print("Container created")
+        
+    def upload_document(self, document: dict):
+        """
+        Upload a document to the container
+        :param document: The document to upload
+        """
+        return self.container.upsert_item(document)
