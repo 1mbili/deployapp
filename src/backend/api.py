5d46c7d6c358b1e5120c5c6de3610ec9d6972dcf
Michal Biliński <m.bilinskimichal@gmail.com>
2025-02-17 14:06:49 +0100

    Add working correct path file add

--- src/backend/api.py
+++ src/backend/api.py
@@ -20,18 +20,18 @@ async def read_item(request: Request, id: str):
 
 @router.get("/upload", response_class=HTMLResponse)
 async def upload_image(request: Request):
-    print(request.headers)
     return templates.TemplateResponse(
         request=request, name="upload.html"
     )
 
 
 @router.post("/upload", status_code=status.HTTP_201_CREATED)
-async def post_recipt_image(file: UploadFile = File(...)):
+async def post_recipt_image(request: Request, file: UploadFile = File(...)):
     if file.content_type not in ["application/pdf", "image/png", "image/jpg", "image/jpeg"]:
         raise HTTPException(400, detail="Invalid file type")
     content = await file.read()
-    az_handler = AzureBlobHandler()
+    container_prefix = f"prod/{request['x-ms-client-principal-name']}"
+    az_handler = AzureBlobHandler(container_prefix)
     print("Uploading file")
     az_handler.upload_blob(file.filename, content)
     return "OK"
